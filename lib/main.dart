86db48e6c1af7a00bf49de6f6e0f410100350e3f
“Ryota <kurokwa.ryota.1124@gmail.com>
2023-09-15 17:05:55 +0900

    test push

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'start_page.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
-  runApp(myapp());
+Future<void> main() async {
+  const scope = ProviderScope(child: Fotografia());
+  runApp(scope);
 }
 
-class myapp extends StatelessWidget {
-  const myapp({super.key});
+class Fotografia extends StatelessWidget {
+  const Fotografia({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: StartPage(),
+      home: Config(),
     );
+  }
+}
+
+class Config extends HookWidget {
+  const Config({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isLogin = useState(false);
+    useEffect(
+      () {
+        // FirebaseAuth.instance.signOut();
+        final currentUser = FirebaseAuth.instance.currentUser;
+        if (currentUser != null) {
+          isLogin.value = true;
+        }
+        return;
+      },
+    );
+
+    return isLogin.value ? loginPage() : loginPage();
   }
 }
